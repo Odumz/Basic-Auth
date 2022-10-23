8c427126ab1b3730a50966987e7523ed4a2bc789
Odumz <odumz20@gmail.com>
2022-10-23 10:37:43 +0100

    update made

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,7 @@
 
 # Install node.js
 sudo apt-get install curl
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get install nodejs -y
 
