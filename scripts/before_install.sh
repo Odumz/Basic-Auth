ec2016b4d97ab1484dd236388b019335cfaa4df5
Odumz <odumz20@gmail.com>
2022-10-23 10:31:30 +0100

    update made

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,14 +2,14 @@
 
 # Install node.js
 sudo apt-get install curl
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get install nodejs -y
 
 # Install nodemon
 # sudo npm install nodemon -g
 
-# Install forever module 
+# Install forever module
 # https://www.npmjs.com/package/forever
 # sudo npm install forever -g
 
